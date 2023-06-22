72fb33cfed2efd831dcad141a66bd7517a458282
kurnickiy <kurnickiy@happy-inc.ru>
2023-06-22 17:41:44 +0300

    первый

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { css } from '../styled-system/css';
+import Todo from './Todo';
+import './App.css'
+
+
+function App() {
+  return(
+    <div className="container">
+      <Todo />
+    </div>    
+  )  
+}
+
+export default App
